efdf5c2694947009f9ade687600548e60ef3f218
pathikritgupta <pathikrit.gupta@gmail.com>
2018-09-03 15:29:37 +0530

    Update Port changes

--- Server.js
+++ Server.js
@@ -55,8 +55,8 @@ app.get('/Main*' ,   function(req,res,next) {
 } );
  
 
-app.listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
 	var options = {
